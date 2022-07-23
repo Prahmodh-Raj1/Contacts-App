cbe73bd0a09d40c3e4e863d4416f8dac2995778b
Prahmodh R <89332177+Prahmodh-R@users.noreply.github.com>
2022-07-23 22:03:58 +0530

    file added

---
+++ contacts.js
@@ -0,0 +1,17 @@
+const contacts = [
+  {
+    name: "Conway",
+    img:
+      "https://ss-i.thgim.com/public/cricket/article38085616.ece/alternates/FREE_1200/devon-conway",
+    ph: "+123 456 789",
+    mail: "conwaydevon.com"
+  },
+  {
+    name: "Pujara",
+    img:
+      "https://static.toiimg.com/thumb/msid-91230097,imgsize-26762,width-400,resizemode-4/91230097.jpg",
+    ph: "+987 654 321",
+    mail: "chetpuj.com"
+  }
+];
+export default contacts;
